873847983fbb90aa20067391fda8a59682fd62d9
mystia04 <s.fujieda.1993@gmail.com>
2015-09-28 18:46:49 +0000

    update

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 require 'sqlite3'
+require 'erubis'
 
 ActiveRecord::Base.configurations = YAML.load_file('db/database.yml')
 ActiveRecord::Base.establish_connection(:development)
@@ -11,9 +12,17 @@ ActiveRecord::Base.establish_connection(:development)
 class Station < ActiveRecord::Base
 end
 
+# XSS対策のエスケープ
+set :erb, :escape_html => true
+
 get '/' do
+    erb :mainpage    
+end
+
+get '/alldatadebugapp' do
+    # デバッグ用データベース全表示
     @stations = Station.all
-    erb :index
+    erb :searchResult
 end
 
 get '/input_main' do
@@ -21,45 +30,111 @@ get '/input_main' do
 end
 
 post '/input_station' do
-    $id_st = params[:id_st]
-    checkid = Station.where("mem_id like ?", $id_st)
-    checkid.each do |sta|
-        @chkid = sta.mem_id
-    end
-    if @chkid == $id_st then
+    
+    # IDのパラメータをサニタイズして小文字化して取得
+    tmpStr = ERB::Util.html_escape(params[:id_st]).downcase
+    
+    # IDパラメータの検証
+    if tmpStr !~ /^[0-9a-z]+$/
         redirect 'idError'
     end
+    
+    # IDのパラメーター受け取り(@id_st)
+    @id_st = tmpStr
+    
+    # ID重複チェック
+    #checkid = Station.where("mem_id like ?", @id_st)
+    
+    #checkid.each do |sta|
+    #    @chkid = sta.mem_id
+    #end
+    
+    #if @chkid == @id_st then
+    
+    # ID重複チェック
+    # existsを使うとキレイにかける
+    if Station.exists?(:mem_id => @id_st)
+        # 重複時処理
+        redirect 'idError'
+    end
+    
+    # 繰り返し用変数
     @num = 0
-    number_st = params[:number_st]
+    
+    # 駅数
+    # 各パラメータをサニタイズして取得
+    number_st = ERB::Util.html_escape(params[:number_st])
     @numst = number_st.to_i
-    $finst = @numst + 2
-    $start_st = params[:start_st]
-    $finish_st = params[:finish_st]
+    @start_st = ERB::Util.html_escape(params[:start_st])
+    @finish_st = ERB::Util.html_escape(params[:finish_st])
+    
+    # いたずら防止(流石に1000駅以上のメモリールートはない)
     if @numst > 1000 then
         redirect 'tooMuchStation'
     end
+    
+    
     erb :input_station
 end
 
-post '/input_station_test' do
-    @num = params[:number_st]
-    erb :test
-end
-
 post '/confirm_station' do
+    # パラメータ受け取り
     stations = params[:st]
+    @id_st = params[:id_st]
+    
+    # 空欄チェック
     checknull = stations.any?{|w| w.empty?}
     if checknull == true then
-        redirect 'error'
+        redirect 'emptyError'
     end
-    station_in = stations.join(",")
-    station_sf = $start_st + "," + $finish_st
+    
+    # 開始終了駅抽出
+    @start_st = ERB::Util.html_escape(params[:start_st])
+    @finish_st = ERB::Util.html_escape(params[:finish_st])
+    @numst = stations.length
+    @num = 0
+    
+    
+    # データベース記録用に記録駅の文字列化
+    @station_in = stations.join(",")
+    
+    # 記号を削除
+    @station_in = @station_in.gsub(/[!"#$%&'()\*\+\-\.\/:;<=>?@\[\\\]^_`{|}~]/,"")
+    
+    # 前構成の残骸(デバッグ用)
+    # station_sf = $start_st + "," + $finish_st
+    # station = Station.new
+    # station.mem_id = $id_st
+    # station.sf_station = station_sf
+    # station.in_station = @station_in
+    # station.save
+    erb :addResult
+end
+
+post '/addFinish' do
+    # パラメータ受け取り
+    id = ERB::Util.html_escape(params[:id_st])
+    start_st = ERB::Util.html_escape(params[:start_st])
+    finish_st = ERB::Util.html_escape(params[:finish_st])
+    station_in = ERB::Util.html_escape(params[:station_in])
+    
+    # 一応再度記号を削除
+    station_in = station_in.gsub(/[!"#$%&'()\*\+\-\.\/:;<=>?@\[\\\]^_`{|}~]/,"")
+    
+    # DB記録前処理
+    sf_station = start_st + "," + finish_st
     station = Station.new
-    station.mem_id = $id_st
-    station.sf_station = station_sf
+    station.mem_id = id
+    station.sf_station = sf_station
     station.in_station = station_in
     station.save
-    "ID:(#{$id_st})に#{$finst}駅登録しました。(開始終了駅[#{station_sf}],駅[#{station_in}])"
+    
+    erb :addCompleate
+end
+
+
+get '/emptyError' do
+    "駅名に空白があります"
 end
 
 get '/error' do
@@ -74,13 +149,19 @@ get '/idError' do
     "ID重複 : 既に同じIDのルートが登録されています"
 end
 
-get '/searchtest' do
+# 検索のとこ
+get '/search' do
+    erb :search
+end 
+
+# 検索結果表示
+post '/searchResult' do
     first = params[:first]
     last = params[:last]
     st_first = first + "," + last
     st_last = last + "," + first
     @stations = Station.where("sf_station like ? or sf_station like ?", st_first, st_last)
-    erb :index
+    erb :searchResult
 end
 
 get '/findtest' do
